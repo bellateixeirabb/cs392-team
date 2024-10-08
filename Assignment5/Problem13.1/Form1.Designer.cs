53e8766aca8d5608440dd949b60fed1a0b99ed36
bellateixeirabb <isabella.teixeira@us.abb.com>
2024-10-08 14:15:10 -0400

    Add files via upload

---
+++ Assignment5/Problem13.1/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Problem13._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TableofInfo = new System.Windows.Forms.DataGridView();
+            this.employeeIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.positionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hourlyPayRateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.employeeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.personnelDataSet = new Problem13._1.PersonnelDataSet();
+            this.employeeTableAdapter = new Problem13._1.PersonnelDataSetTableAdapters.EmployeeTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.TableofInfo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personnelDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TableofInfo
+            // 
+            this.TableofInfo.AutoGenerateColumns = false;
+            this.TableofInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TableofInfo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.employeeIdDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.positionDataGridViewTextBoxColumn,
+            this.hourlyPayRateDataGridViewTextBoxColumn});
+            this.TableofInfo.DataSource = this.employeeBindingSource;
+            this.TableofInfo.Location = new System.Drawing.Point(125, 76);
+            this.TableofInfo.Name = "TableofInfo";
+            this.TableofInfo.RowHeadersWidth = 51;
+            this.TableofInfo.RowTemplate.Height = 24;
+            this.TableofInfo.Size = new System.Drawing.Size(554, 216);
+            this.TableofInfo.TabIndex = 0;
+            this.TableofInfo.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.TableofInfo_CellContentClick);
+            // 
+            // employeeIdDataGridViewTextBoxColumn
+            // 
+            this.employeeIdDataGridViewTextBoxColumn.DataPropertyName = "Employee Id";
+            this.employeeIdDataGridViewTextBoxColumn.HeaderText = "Employee Id";
+            this.employeeIdDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.employeeIdDataGridViewTextBoxColumn.Name = "employeeIdDataGridViewTextBoxColumn";
+            this.employeeIdDataGridViewTextBoxColumn.ReadOnly = true;
+            this.employeeIdDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "name";
+            this.nameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // positionDataGridViewTextBoxColumn
+            // 
+            this.positionDataGridViewTextBoxColumn.DataPropertyName = "position";
+            this.positionDataGridViewTextBoxColumn.HeaderText = "position";
+            this.positionDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.positionDataGridViewTextBoxColumn.Name = "positionDataGridViewTextBoxColumn";
+            this.positionDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // hourlyPayRateDataGridViewTextBoxColumn
+            // 
+            this.hourlyPayRateDataGridViewTextBoxColumn.DataPropertyName = "hourly pay rate";
+            this.hourlyPayRateDataGridViewTextBoxColumn.HeaderText = "hourly pay rate";
+            this.hourlyPayRateDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.hourlyPayRateDataGridViewTextBoxColumn.Name = "hourlyPayRateDataGridViewTextBoxColumn";
+            this.hourlyPayRateDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // employeeBindingSource
+            // 
+            this.employeeBindingSource.DataMember = "Employee";
+            this.employeeBindingSource.DataSource = this.personnelDataSet;
+            // 
+            // personnelDataSet
+            // 
+            this.personnelDataSet.DataSetName = "PersonnelDataSet";
+            this.personnelDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // employeeTableAdapter
+            // 
+            this.employeeTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 553);
+            this.Controls.Add(this.TableofInfo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.TableofInfo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personnelDataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView TableofInfo;
+        private PersonnelDataSet personnelDataSet;
+        private System.Windows.Forms.BindingSource employeeBindingSource;
+        private PersonnelDataSetTableAdapters.EmployeeTableAdapter employeeTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn employeeIdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn positionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hourlyPayRateDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Assignment5/Problem13.4/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Problem13._4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // initiates the table as a 
+            this.employeeTableAdapter.Fill(this.personnelDataSet.Employee);
+
+        }
+
+        private void PersonFind_Click(object sender, EventArgs e)
+        {
+            //Runs the SQL SELECT method that will only show lines containing names that contain the string fragment the user input
+            this.employeeTableAdapter.FillByName
+                (this.personnelDataSet.Employee, nameSearch.Text);
+        }
+    }
+}
